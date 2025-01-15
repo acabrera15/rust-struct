6f61fa361dfd2dcd391b72c83c844f5cc5052d09
Andrew Cabrera <andrewcabrera@Mac.attlocal.net>
2025-01-15 15:03:31 -0800

    feature(update employee): added abilty to input value index to change

--- src/main.rs
+++ src/main.rs
@@ -47,7 +47,13 @@ struct Department {
 // }
 
 fn main() {
-    create_employee();
+    let mut employees = vec![Employee {
+        name: String::from("Andrew"),
+        id: 12,
+        department: String::from("Business"),
+        salary: 10000.00,
+    }];
+    update_employee(&mut employees);
 }
 fn create_employee() -> Employee {
     // capture name
@@ -103,7 +109,7 @@ fn list_employees(employees: &Vec<Employee>) {
 
 fn update_employee(employees: &mut Vec<Employee>) {
     let mut employee_id = String::new();
-    let mut employee_id_num: u32;
+    let employee_id_num: u32;
     // get use input
     loop {
         println!("Enter employee's id you would like to update");
@@ -123,6 +129,42 @@ fn update_employee(employees: &mut Vec<Employee>) {
     }
 
     if let Some(found_employee) = employees.iter().find(|&x| x.id == employee_id_num) {
+        println!("Enter the number of the value you like to update?");
+        println!("1. name: {}", found_employee.name);
+        println!("2. department: {}", found_employee.department);
+        println!("3. salary: {}", found_employee.salary);
+
+        let mut item_to_change = String::new();
+        let mut item_to_change_num: u8;
+
+        loop {
+            io::stdin()
+                .read_line(&mut item_to_change)
+                .expect("Unable to read line");
+
+            match item_to_change.trim().parse() {
+                Ok(num) => item_to_change_num = num,
+                Err(_) => {
+                    println!("Please enter a valid number");
+                    continue;
+                }
+            }
+
+            if item_to_change_num > 3 || item_to_change_num < 1 {
+                println!("Enter a value from 1-3");
+                continue;
+            }
+
+            break;
+        }
+
+        if item_to_change_num == 1 {
+            println!("changing name");
+        } else if item_to_change_num == 2 {
+            println!("changing department");
+        } else if item_to_change_num == 3 {
+            println!("changing Salary");
+        }
     } else {
         println!("An employee with id: {} as not found. Please look at the employee list to use a valid ID", employee_id);
     }
